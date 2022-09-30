29536f07ba23edf106c67ba3aa7dc6b7ab4f7f96
장미림 <109326275+Mirimy@users.noreply.github.com>
2022-09-30 17:15:58 +0900

    [D1] Title: 자릿수 더하기, Time: 132 ms, Memory: 58,372 KB -BaekjoonHub

---
+++ SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.py
@@ -0,0 +1,10 @@
+a = str(input())
+
+if len(a) == 1 :
+    print(a[0])
+elif len(a) == 2 :
+    print(int(a[0]) + int(a[1]))
+elif len(a) == 3 :
+    print(int(a[0]) + int(a[1]) + int(a[2]))
+else :
+    print(int(a[0]) + int(a[1]) + int(a[2]) + int(a[3]))
