a6d4586ffe8a14234ff1e4491b56f91b9b08572f
장미림 <109326275+Mirimy@users.noreply.github.com>
2022-10-24 14:25:10 +0900

    [Silver II] Title: 마인크래프트, Time: 208 ms, Memory: 125096 KB -BaekjoonHub

---
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -0,0 +1,37 @@
+import sys
+from collections import Counter
+
+N, M, BB = map(int, input().split())
+arr = []
+for _ in range(N):
+    arr += list(map(int, sys.stdin.readline().strip().split()))
+
+
+cnt = Counter(arr).most_common()
+cnt_sort = sorted(cnt, reverse=True)
+
+min_t = [256 * N * M * 2, 0]
+for i in range(min(arr), max(arr) + 1):
+    t = 0
+    B = BB
+    for j in range(len(cnt)):
+        bk = cnt_sort[j]
+        if i == bk[0]:
+            continue
+        else:
+            if bk[0] > i:
+                B += (bk[0] - i) * bk[1]
+                t += ((bk[0] - i) * bk[1]) * 2
+            elif bk[0] < i:
+                B -= (i - bk[0]) * bk[1]
+                t += (i - bk[0]) * bk[1]
+        if B < 0:
+            t = -1
+            break
+    if t != -1:
+        if t == min_t[0] and i > min_t[1]:
+            min_t = [t, i]
+        elif t < min_t[0]:
+            min_t = [t, i]
+
+print(min_t[0], min_t[1])
