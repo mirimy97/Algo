cb8900cf9986f05badafa3e668ccae0afeb96db8
장미림 <109326275+Mirimy@users.noreply.github.com>
2022-11-08 10:05:36 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 156 ms, Memory: 119324 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+N, K = map(int, input().split())
+if N >= K:
+    print(N - K)
+else:
+    visited = [0] * (K+2)
+    q = deque([N])
+    visited[N] = 1
+    while visited[K] == 0:
+        S = q.popleft()
+
+        E = 2 * S
+        if 0 <= E <= K + 1 and not visited[E]:
+            q.append(E)
+            visited[E] = visited[S]
+            
+        for E in [S-1, S+1]:
+            if 0 <= E <= K+1 and not visited[E]:
+                q.append(E)
+                visited[E] = visited[S] + 1
+    print(visited[K] - 1)
