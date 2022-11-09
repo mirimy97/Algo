3f8ba4caf2ca0517c34acf3d3034561e1f27a18a
장미림 <109326275+Mirimy@users.noreply.github.com>
2022-11-09 14:27:56 +0900

    [Silver III] Title: 2×n 타일링, Time: 216 ms, Memory: 118324 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -3,8 +3,14 @@ import math
 n = int(input())
 T = n // 2  # 최대 2개짜리 개수
 result = 0
+
+facto = [0] * (n+1)
+for i in range(1, n+1):
+    facto[i] = math.factorial(i)
+facto[0] = 1
+
 for two in range(0, T+1):
     one = n - (2 * two)
-    result += math.factorial(one + two) // (math.factorial(one) * math.factorial(two))
+    result += facto[one + two] // (facto[one] * facto[two])
 
 print(result%10007)
