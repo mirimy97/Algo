b433fae5bf3ebc7a3afe88b2726a3b63000ecf0d
mirimy97 <121851585+mirimy97@users.noreply.github.com>
2023-07-19 23:02:54 +0900

    [Silver II] Title: 그르다 김가놈, Time: 908 ms, Memory: 213400 KB -BaekjoonHub

---
+++ 백준/Silver/18113. 그르다 김가놈/그르다 김가놈.py
@@ -0,0 +1,32 @@
+import sys
+
+N, K, M = map(int, sys.stdin.readline().split())
+L = []
+for i in range(N):
+    long = int(sys.stdin.readline())
+    if long <= K:
+        continue
+    elif long < 2 * K:
+        L.append(long - K)
+    else:
+        L.append(long - (2 * K))
+
+if len(L) == 0:
+    print(-1)
+    exit(0)
+
+P = -1
+l,r = 1,max(L)
+
+while l <= r:
+
+    m = (l+r)//2 #조각 김밥의 최대 길이
+    total = sum([i//m for i in L])
+
+    if total < M:
+        r = m-1
+    else:
+        l = m+1
+        P = m
+
+print(P)
