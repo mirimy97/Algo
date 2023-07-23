9d050e949cd284311fc27148179f7b0fcf17009e
mirimy97 <121851585+mirimy97@users.noreply.github.com>
2023-07-23 23:45:25 +0900

    [Silver III] Title: 모든 순열, Time: 2904 ms, Memory: 80328 KB -BaekjoonHub

---
+++ 백준/Silver/10974. 모든 순열/모든 순열.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static boolean [] chosen;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        chosen = new boolean[N];
+        Arrays.fill(chosen, false);
+        int [] p = new int[N];
+        perm(N, 0, p);
+
+        br.close();
+
+    }
+
+    public static void perm(int N, int cnt, int [] p) {
+        if (cnt == N) {
+            //  p 출력
+            for (int j = 0; j < N; j++) {
+                System.out.print(p[j]+" ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            if (chosen[i] == true) continue;
+            chosen[i] = true;
+            p[cnt] = i + 1;
+            perm(N, cnt + 1, p);
+            chosen[i] = false;
+        }
+    }
+
+
+}
