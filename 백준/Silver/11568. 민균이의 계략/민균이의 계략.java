7e62e6d4b22fdbf70f7ffbb9b3fbe03177d61a72
mirimy97 <121851585+mirimy97@users.noreply.github.com>
2023-07-26 11:39:25 +0900

    [Silver II] Title: 민균이의 계략, Time: 156 ms, Memory: 14712 KB -BaekjoonHub

--- 백준/Silver/11568. 민균이의 계략/민균이의 계략.java
+++ 백준/Silver/11568. 민균이의 계략/민균이의 계략.java
@@ -1,32 +1,29 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[] nums = new int[N];
-        int[] dp = new int[N]; // dp[i]: i번째 원소를 마지막으로 하는 LIS의 길이
-        Arrays.fill(dp, 1);
-
-        String[] input = br.readLine().split(" ");
+        int[] cnts = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            nums[i] = Integer.parseInt(input[i]);
+            nums[i] = Integer.parseInt(st.nextToken());
+            cnts[i] = 1;
         }
 
-        for (int i = 1; i < N; i++) {
+        int i = 1;
+        int max_cnt = 1;
+        while (i < N) {
             for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
+                if (nums[j] < nums[i]) cnts[i] = Math.max(cnts[i], cnts[j] + 1);
             }
+            if (max_cnt < cnts[i]) max_cnt = cnts[i];
+            i++;
         }
-
-        int maxLen = Arrays.stream(dp).max().orElse(0);
-        System.out.println(maxLen);
-
+        System.out.println(max_cnt);
         br.close();
     }
 }
