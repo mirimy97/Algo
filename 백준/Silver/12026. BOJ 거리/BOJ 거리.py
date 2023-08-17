edcf501f5fb620ebde7062c75944cbbebcbe5d3a
mirimy97 <121851585+mirimy97@users.noreply.github.com>
2023-08-17 16:15:09 +0900

    [Silver I] Title: BOJ 거리, Time: 144 ms, Memory: 115268 KB -BaekjoonHub

--- 백준/Silver/12026. BOJ 거리/BOJ 거리.py
+++ 백준/Silver/12026. BOJ 거리/BOJ 거리.py
@@ -8,7 +8,7 @@ dp = [1000000] * N
 dp[0] = 0
 for i in range(1, N):
     for j in range(i, -1, -1):
-        if map[nodes[j]] == nodes[i]:
+        if map[nodes[j]] == nodes[i] and dp[j] != 1000000:
             dp[i] = min(dp[i], dp[j] + ((i - j) ** 2))
 if dp[-1] == 1000000:
     print(-1)
