1b7aed5a02104b9dc131644f95c261450e83516b
mirimy97 <121851585+mirimy97@users.noreply.github.com>
2023-08-31 02:48:51 +0900

    [Gold I] Title: 달빛 여우, Time: 928 ms, Memory: 77484 KB -BaekjoonHub

---
+++ 백준/Gold/16118. 달빛 여우/달빛 여우.py
@@ -0,0 +1,41 @@
+import sys
+from heapq import heappush, heappop
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+arr = [[] for _ in range(N + 1)]
+for _ in range(M):
+    a, b, d = map(int, input().split())
+    arr[a].append((b, d))
+    arr[b].append((a, d))
+
+# # 여우 (정속)
+v_fox = [float('inf')] * (N + 1)
+v_fox[1] = 0
+q = [(0, 1)]   # (cost, node)
+while q:
+    cost, s = heappop(q)
+    if cost > v_fox[s]: continue
+    for e, d in arr[s]:
+        new_cost = cost + d
+        if v_fox[e] > new_cost:
+            heappush(q, (new_cost, e))
+            v_fox[e] = new_cost
+
+# 늑대 (변속)
+v_wolf = [[float('inf'), float('inf')] for _ in range(N + 1)]   # [False일때, True일때]
+q = [(0, 1, True)]    # (cost, node, t:가속/f:감속)
+v_wolf[1][0] = 0
+while q:
+    cost, s, fast = heappop(q)
+    if cost > v_wolf[s][not fast]: continue
+    for e, d in arr[s]:
+        if fast:
+            new_cost = cost + (d / 2)
+        else:
+            new_cost = cost + (d * 2)
+        if v_wolf[e][fast] > new_cost:
+            v_wolf[e][fast] = new_cost
+            heappush(q, (new_cost, e, not fast))
+
+print(sum(1 for i in range(2, N+1) if v_fox[i] < min(v_wolf[i][0], v_wolf[i][1])))
