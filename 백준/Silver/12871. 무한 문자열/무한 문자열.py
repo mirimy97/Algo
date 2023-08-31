8a591208a9ef8f1d6141abb2a2bfc883fc7736db
mirimy97 <121851585+mirimy97@users.noreply.github.com>
2023-08-31 17:06:20 +0900

    [Silver V] Title: 무한 문자열, Time: 124 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/12871. 무한 문자열/무한 문자열.py
+++ 백준/Silver/12871. 무한 문자열/무한 문자열.py
@@ -1,14 +1,6 @@
-import sys
-input = sys.stdin.readline
-s = input().rstrip()
-t = input().rstrip()
-if len(s) > len(t):
-    s, t = t, s
-len_s, len_t = len(s), len(t)
-idx = 0
-while idx < (len_t * 2):
-    if s[idx % len_s] != t[idx % len_t]:
-        print(0)
-        sys.exit(0)
-    idx += 1
-print(1)
+import sys  
+input = sys.stdin.readline  
+
+fs = input().strip()  
+ft = input().strip()  
+print(1 if fs*len(ft) == ft*len(fs) else 0)
