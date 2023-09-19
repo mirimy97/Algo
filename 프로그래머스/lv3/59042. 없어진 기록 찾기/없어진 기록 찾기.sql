f549710287bf7f93e93331f140147904e9995f66
mirimy97 <121851585+mirimy97@users.noreply.github.com>
2023-09-20 03:45:23 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,8 +1,7 @@
--- 코드를 입력하세요
-SELECT b.animal_id, b.name
-from animal_ins a
-right join animal_outs b
-on a.animal_id = b.animal_id
-where b.animal_id not in (select animal_id
-                         from animal_ins)
-order by b.animal_id, b.name;
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_OUTS
+WHERE ANIMAL_ID NOT IN (SELECT A.ANIMAL_ID
+                       FROM ANIMAL_INS A
+                       INNER JOIN ANIMAL_OUTS B
+                       ON A.ANIMAL_ID = B.ANIMAL_ID)
+ORDER BY ANIMAL_ID
