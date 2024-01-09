5cd88e113e04fddf18bb6144501d449c691e5d91
mirimy97 <121851585+mirimy97@users.noreply.github.com>
2024-01-09 17:43:46 +0900

    [Gold V] Title: 태상이의 훈련소 생활, Time: 284 ms, Memory: 41932 KB -BaekjoonHub

---
+++ 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+# 연병장 [] N칸, 조교 M명
+H = [0] + list(map(int, input().split()))
+memo = [0] * (N + 2)
+for _ in range(M):
+    a, b, k = map(int, input().split())
+    memo[a] += k
+    memo[b + 1] -= k
+for i in range(1, N + 1):
+    memo[i] += memo[i - 1]
+    print(H[i] + memo[i], end=" ")
