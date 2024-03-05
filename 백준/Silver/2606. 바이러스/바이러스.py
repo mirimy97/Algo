0d5049a85a8b1a803a1652fa251f3cc8fa6ee716
mirimy97 <121851585+mirimy97@users.noreply.github.com>
2024-03-05 18:38:24 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,24 +1,24 @@
 import sys
-from collections import deque
+input = sys.stdin.readline
 
-C = int(input())
 N = int(input())
-arr = [[0] * (C+1) for _ in range(C+1)]
-for _ in range(N):
-    c1, c2 = map(int, sys.stdin.readline().split())
-    arr[c1][c2] = 1
-    arr[c2][c1] = 1
+M = int(input())
+arr = [[] for _ in range(N + 1)]
 
-q = deque()
-visited = [0] * (C+1)
-S = 1
-while True:
-    for E in range(1, C+1):
-        if arr[S][E] == 1 and not visited[E]:
-            visited[E] = 1
-            q.append(E)
-    if q:
-        S = q.pop()
-    else:
-        break
-print(sum(visited) - 1)
+for _ in range(M):
+    a, b = map(int, input().split())
+    arr[a].append(b)
+    arr[b].append(a)
+
+v = [0] * (N + 1)
+q = [1]
+v[1] = 1
+cnt = 0
+while q:
+    s = q.pop()
+    for e in arr[s]:
+        if not v[e]:
+            q.append(e)
+            v[e] = 1
+            cnt += 1
+print(cnt)
